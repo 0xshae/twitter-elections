b33017ba549fd3dae2ad1911ef2e352f51c72cad
0xshae <shagun.prasad28@gmail.com>
2025-04-05 16:58:31 +0530

    Adding voting program, tests

--- anchor/tests/votingdapp.spec.ts
+++ anchor/tests/votingdapp.spec.ts
@@ -1,76 +1,119 @@
-import * as anchor from '@coral-xyz/anchor'
-import { Program } from '@coral-xyz/anchor'
-import { Keypair } from '@solana/web3.js'
-import { Votingdapp } from '../target/types/votingdapp'
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Votingdapp } from '../target/types/votingdapp';
 
-describe('votingdapp', () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
+const IDL = require("../target/idl/votingdapp.json");
+const votingAddress = new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
-  const program = anchor.workspace.Votingdapp as Program<Votingdapp>
+describe('Votingdapp', () => {
+  let context;
+  let provider;
+  let votingProgram: anchor.Program<Votingdapp>;
 
-  const votingdappKeypair = Keypair.generate()
+  beforeAll( async () => {
+    context = await startAnchor("", [{name: "votingdapp", programId: votingAddress}], []);
+	  provider = new BankrunProvider(context);
+    votingProgram = new Program<Votingdapp>(
+		IDL,
+		provider,
+	); 
+  }) 
 
-  it('Initialize Votingdapp', async () => {
-    await program.methods
-      .initialize()
-      .accounts({
-        votingdapp: votingdappKeypair.publicKey,
-        payer: payer.publicKey,
-      })
-      .signers([votingdappKeypair])
-      .rpc()
 
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
+//test for initializing poll
+  it('Initialize Poll', async () => {
 
-    expect(currentCount.count).toEqual(0)
-  })
+  await votingProgram.methods.initializePoll(
+    new anchor.BN(1),
+    "Who is your favorite engineer on X?",
+    new anchor.BN(0),
+    new anchor.BN(1913035500),
+  ).rpc();
 
-  it('Increment Votingdapp', async () => {
-    await program.methods.increment().accounts({ votingdapp: votingdappKeypair.publicKey }).rpc()
+  const [pollAddress] = PublicKey.findProgramAddressSync(
+    [new anchor.BN(1).toArrayLike(Buffer, 'le', 8)],
+    votingAddress,
+  )
 
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
+  const poll = await votingProgram.account.poll.fetch(pollAddress); 
 
-    expect(currentCount.count).toEqual(1)
-  })
+  console.log(poll);
 
-  it('Increment Votingdapp Again', async () => {
-    await program.methods.increment().accounts({ votingdapp: votingdappKeypair.publicKey }).rpc()
+  expect(poll.pollId.toNumber()).toEqual(1);
+  expect(poll.description).toEqual("Who is your favorite engineer on X?");
+  expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
+  
+  });
 
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
+  //test for initializing candidate
 
-    expect(currentCount.count).toEqual(2)
-  })
+  it('Initialize Candidate', async () => {
+    await votingProgram.methods.initializeCandidate(
+      "yacineMTB",
+      new anchor.BN(1),
+    ).rpc();
 
-  it('Decrement Votingdapp', async () => {
-    await program.methods.decrement().accounts({ votingdapp: votingdappKeypair.publicKey }).rpc()
+    //create PDA for yacineMTBCandidate
+    const [yacineAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("yacineMTB")],
+      votingAddress,
+    );
+    
+    //fetching account details using PDA
+    const yacineMTBCandidate = await votingProgram.account.candidate.fetch(yacineAddress);
+  
+    console.log(yacineMTBCandidate);
+    expect(yacineMTBCandidate.candidateVotes.toNumber()).toEqual(0);
 
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
 
-    expect(currentCount.count).toEqual(1)
-  })
 
-  it('Set votingdapp value', async () => {
-    await program.methods.set(42).accounts({ votingdapp: votingdappKeypair.publicKey }).rpc()
+    //CANDIDATE #2
+    await votingProgram.methods.initializeCandidate(
+      "Primeagen",
+      new anchor.BN(1),
+    ).rpc();
 
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
+    const [primeagenAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Primeagen")],
+      votingAddress,
+    );
+  
+    const primeagenCandidate = await votingProgram.account.candidate.fetch(primeagenAddress);
+  
+    console.log(primeagenCandidate);
+    expect(primeagenCandidate.candidateVotes.toNumber()).toEqual(0);
 
-    expect(currentCount.count).toEqual(42)
-  })
+  });
 
-  it('Set close the votingdapp account', async () => {
-    await program.methods
-      .close()
-      .accounts({
-        payer: payer.publicKey,
-        votingdapp: votingdappKeypair.publicKey,
-      })
-      .rpc()
 
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.votingdapp.fetchNullable(votingdappKeypair.publicKey)
-    expect(userAccount).toBeNull()
-  })
-})
+  const [primeAddress] = PublicKey.findProgramAddressSync(
+    [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Primeagen")],
+    votingAddress,
+  );
+
+  //test for voting
+
+  it('vote', async () => {
+    await votingProgram.methods.vote(
+      "yacineMTB",
+      new anchor.BN(1),
+    ).rpc();
+
+    const [yacineAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("yacineMTB")],
+      votingAddress,
+    );
+    
+    //fetching account details using PDA
+    const yacineMTBCandidate = await votingProgram.account.candidate.fetch(yacineAddress);
+    
+    console.log(yacineMTBCandidate);
+    expect(yacineMTBCandidate.candidateVotes.toNumber()).toEqual(1);
+  });
+
+
+  
+
+});
